c09cd8fcb6a5707c7cb22e0b87e325892d3f884b
Divya Chinamilli <chinamil@usc.edu>
2021-05-02 10:13:41 -0700

    ultrasonic sensor

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -28,11 +28,52 @@ import grovepi
 is, if you run `python3 grovepi_sensors.py` in terminal, this if-statement will 
 be true"""
 if __name__ == '__main__':
-    PORT = 4    # D4
+    # Connect the LED to digital port D5
+    # SIG,NC,VCC,GND
+    led = 5
+
+    grovepi.pinMode(potentiometer,"INPUT")
+    grovepi.pinMode(led,"OUTPUT")
+    time.sleep(1)
+
+    # Reference voltage of ADC is 5v
+    adc_ref = 5
+
+    # Vcc of the grove interface is normally 5v
+    grove_vcc = 5
+
+    # Full value of the rotary angle is 300 degrees, as per it's specs (0 to 300)
+    full_angle = 300
 
     while True:
+        try:
+            # Read sensor value from potentiometer
+            sensor_value = grovepi.analogRead(potentiometer)
+
+            # Calculate voltage
+            voltage = round((float)(sensor_value) * adc_ref / 1023, 2)
+
+            # Calculate rotation in degrees (0 to 300)
+            degrees = round((voltage * full_angle) / grove_vcc, 2)
+
+            # Calculate LED brightess (0 to 255) from degrees (0 to 300)
+            brightness = int(degrees / full_angle * 255)
+
+            # Give PWM output to LED
+            grovepi.analogWrite(led,brightness)
+
+            print("sensor_value = %d voltage = %.2f degrees = %.1f brightness = %d" %(sensor_value, voltage, degrees, brightness))
+        except KeyboardInterrupt:
+            grovepi.analogWrite(led,0)
+            break
+        except IOError:
+            print ("Error")
+
+    #PORT = 4    # D4
+
+    #while True:
         #So we do not poll the sensors too quickly which may introduce noise,
         #sleep for a reasonable time of 200ms between each iteration.
-        time.sleep(0.2)
+        #time.sleep(0.2)
 
-        print(grovepi.ultrasonicRead(PORT))
+        #print(grovepi.ultrasonicRead(PORT)
